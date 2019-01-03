339595804f6061012e8b8c15ff00fe513a05117d
Marie Fernandez <nico.le.borgne@gmail.com>
2019-01-03 11:34:56 +0100

    First commit

---
+++ bot.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import tweepy
+from secrets import *
+import requests
+
+# create OauthHandler instance
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_secret)
+# API instance
+api = tweepy.API(auth)
+
+
+def show_tweets(api):
+    # show public tweets
+    # problem : can't have emojis yet
+    public_tweets = api.home_timeline()
+    for tweet in public_tweets :
+        print(tweet.text.encode('utf-8',errors='ignore'))
+
+def get_followers(api):
+    # get followers
+    user = api.get_user('ratpidf')
+    for friend in user.friends():
+        print(friend.screen_name)
+
+
+def getTraffic(train,line):
+    if(train.lower() == 'rer'):
+        train1 = 'rers'
+    elif(train.lower() == 'metro' or train.lower() == 'métro'):
+        train1 = 'metros'
+    
+    url = ('https://api-ratp.pierre-grimaud.fr/v3/traffic/{}/{}'.format(train1.lower(),line.lower()))
+    resp = requests.get(url=url)
+    title = resp.json()['result']['title']
+    message = resp.json()['result']['message']
+    return ('{} {} : {} - {}'.format(train.upper(),line.upper(),title,message))
+
+class BotStreamer(tweepy.StreamListener):
+
+    def on_status(self,status):
+        username = status.user.screen_name
+        status_id = status.id
+
+        print('{} : {}'.format(username,status.text).encode('utf-8',errors='ignore'))
+        string = status.text.split()
+        try :
+            traffic = getTraffic(string[1],string[2])
+            api.update_status(status='@{} {}'.format(username,traffic),in_reply_to_status_id=status.id)
+        except Exception as e:
+            print(e)
+
+myStreamListener = BotStreamer()
+stream = tweepy.Stream(auth,myStreamListener)
+stream.filter(track=['@ratpidf'])
---
+++ secrets.py
@@ -0,0 +1,4 @@
+consumer_key = 'YOUR CONSUMER KEY'
+consumer_secret = 'YOUR CONSUMER SECRET KEY'
+access_token = 'YOUR ACCESS TOKEN KEY'
+access_secret = 'YOUR ACCESS TOKEN SECRET KEY'
